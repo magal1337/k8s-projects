508ea5b0d612767e924976042a8e67b058f7dae1
magal1337 <lucas.m.e.ferreira@gmail.com>
2021-11-06 12:37:06 -0300

    Adjusting test dag and values.yaml v.0.0.1

--- k8s-arch/airflow-arch/dags/dag_test.py
+++ k8s-arch/airflow-arch/dags/dag_test.py
@@ -5,10 +5,15 @@ from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
     return 'Hello world from first Airflow DAG!'
+default_args = {
+'owner':'ferreira.lucas',
+'start_date': '2021-11-05',
+'email': ['lucas.m.e.ferreira@gmail.com'],
+'email_on_failure': False,
+'email_on_success': False
+}
 
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date="2021-11-05", catchup=False)
+dag = DAG('hello_world', description='Hello World DAG',schedule_interval='0 12 * * *', default_args=default_args, catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
