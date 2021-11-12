b3588315d8fd9284df3ae4eec86fe210475d6d30
magal1337 <lucas.m.e.ferreira@gmail.com>
2021-11-11 21:34:38 -0300

    Adjust stock dag v1.0.0

--- k8s-arch/airflow-arch/dags/dag_stock_mkt.py
+++ k8s-arch/airflow-arch/dags/dag_stock_mkt.py
@@ -16,10 +16,10 @@ default_args = {
 }
 
 def get_stock_mkt_data_and_send_to_s3(symbol):
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&outputsize=full&apikey={{var.alpha_vantage_secret}}"
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&outputsize=full&apikey={{var.value.alpha_vantage_secret}}"
     response = requests.get(url)
     data = response.json()
-    s3 = boto3.resource('s3',aws_access_key_id='{{var.minio_key}}', aws_secret_access_key='{{var.minio_secret_key}}')
+    s3 = boto3.resource('s3',aws_access_key_id='{{var.value.minio_key}}', aws_secret_access_key='{{var.value.minio_secret_key}}')
     today = datetime.today().strftime('%Y-%m-%d')
     s3object = s3.Object('stock-market', f'lz/{symbol}-{today}.json')
 
