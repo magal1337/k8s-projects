1a52fd2300580ada9a9b1c3108c6eb68dfc86921
magal1337 <lucas.m.e.ferreira@gmail.com>
2021-11-11 23:00:41 -0300

    Adjust stock mkt dag 1.1.2

--- k8s-arch/airflow-arch/dags/dag_stock_mkt.py
+++ k8s-arch/airflow-arch/dags/dag_stock_mkt.py
@@ -20,11 +20,11 @@ default_args = {
 'tags': ['STOCK','API-REST'],
 }
 
+api_token = Variable.get("alpha_vantage_secret")
+minio_id = Variable.get("minio_key")
+minio_key = Variable.get("minio_secret_key")
 
 def get_stock_mkt_data_and_send_to_s3(symbol):
-    api_token = Variable.get("alpha_vantage_secret")
-    minio_id = Variable.get("minio_key")
-    minio_key = Variable.get("minio_secret_key")
     url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&outputsize=full&apikey={api_token}"
     response = requests.get(url, stream=True)
     data = io.BytesIO(response.content)
@@ -37,13 +37,14 @@ def get_stock_mkt_data_and_send_to_s3(symbol):
     )
     
     today = datetime.today().strftime('%Y-%m-%d')
-    return client.put_object(
+    client.put_object(
         'stock-market',
         f'lz/{symbol}-{today}.json',
         data,
         length=content_length,
         content_type='application/json'
     )
+    return logging.info(f'Extract from {symbol} completed!!!')
 
     
 
